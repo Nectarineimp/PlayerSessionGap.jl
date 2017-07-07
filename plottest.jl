a31e84580ad41fdada865179f60fda15dc3f1a55
Peter Mancini <pete@nectarineimp.com>
2017-07-07 17:49:31 -0500

    Last minute bug fix

--- plottest.jl
+++ plottest.jl
@@ -1,6 +1,6 @@
 using Gadfly
 
-samplesize = 40
+samplesize = 100
 coord = Coord.cartesian(xmin=0.0, xmax=1.0, ymin=0.0, ymax=1.0)
 xsc = Scale.x_continuous(minvalue=0.0,maxvalue=0.0)
 ysc = Scale.y_continuous(minvalue=0.0,maxvalue=1.0)
@@ -10,4 +10,5 @@ Data[:XY] = Data[:X] ./ Data[:Y]
 
 layerAB = layer(Data, x=repeat([0], inner=samplesize), y=repeat([0], inner=samplesize), xend=:X, yend=:Y, color=:Y, Geom.segment)
 
-plot(layerAB, xsc, ysc, colsc, coord)
+p = plot(layerAB, xsc, ysc, colsc, coord)
+draw(PNG("cossim.png", 6inch, 6inch), p)
