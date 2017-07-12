53f19492631927a30712d87bda59da1b3229451d
Peter Mancini <pete@nectarineimp.com>
2017-07-12 16:56:47 -0500

    Modifications, per Roger's input.

--- PSG_01.jl
+++ PSG_01.jl
@@ -14,17 +14,20 @@ function session_similarity(x::Int)
     println(STDERR, "Index value must be > 1 and less than or equal to ", nrow(df))
     exit(-1)
   end
-
-  a = Array(df[x-1,[:cashplayed,	:cashin, :ticketin, :bet_freq, :avg_bet]])
-  b = Array(df[x,[:cashplayed,	:cashin, :ticketin, :bet_freq, :avg_bet]])
+  a = Array(df[x-1,[:cashin, :ticketin, :bet_freq, :avg_bet]])
+  b = Array(df[x,[:cashin, :ticketin, :bet_freq, :avg_bet]])
+  a = [a[1] + a[2], a[3], a[4]]
+  b = [b[1] + b[2], b[3], b[4]]
   return cossim(a,b)
 end
 
 function session_similarity(x::UnitRange{Int})
   return_array = Array{Float64}(0)
   for y in x
-    a = Array(df[y,[:cashplayed,	:cashin, :ticketin, :bet_freq, :avg_bet]])
-    b = Array(df[y+1,[:cashplayed,	:cashin, :ticketin, :bet_freq, :avg_bet]])
+    a = Array(df[y,[:cashin, :ticketin, :bet_freq, :avg_bet]])
+    b = Array(df[y+1,[:cashin, :ticketin, :bet_freq, :avg_bet]])
+    a = [a[1] + a[2], a[3], a[4]]
+    b = [b[1] + b[2], b[3], b[4]]
     push!(return_array, cossim(a,b))
   end
   return return_array
--- Plots_test.jl
+++ Plots_test.jl
@@ -1,21 +1,28 @@
-using Gadfly
+using Gadfly, DataFrames, DataArrays
+df = readtable("C:/Users/Peter.Mancini/Documents/Datafiles/playersessiongap_post.csv")
 
-# heatmap by hour
-
-hmdata = by(df, [:hour], x -> mean(x[:similarity]))
-hmdata2 = Array(trunc(Int64, reshape(hmdata[:x1], 6,4) * 100))
-
-p1 = plot(z=hmdata2, x=1:6, y=1:4, Geom.contour)
-
-p2 = plot(x=0:23, y=hmdata[:x1], Geom.point, Geom.smooth(method=:loess, smoothing=0.9))
-
-draw(SVG("Mean likelihood of stickiness by hour.svg", 10inch, 5inch), p2)
+light_panel = Theme(
+    panel_fill="white",
+    default_color="darkblue",
+    panel_stroke="darkblue",
+    background_color="old lace")
 
 hmdata3 = by(df, [:hour], x -> [mean(x[:similarity]) std(x[:similarity])])
 Ns = by(df, [:hour], x-> size(x)[1])
 ymins = hmdata3[:x1] .- (1.96 * Array(hmdata3[:x2]) ./ Array(sqrt(Ns[:x1])))
 ymaxs = hmdata3[:x1] .+ (1.96 * Array(hmdata3[:x2]) ./ Array(sqrt(Ns[:x1])))
 
-p3 = plot(x=0:23, y=hmdata[:x1], ymin=ymins, ymax = ymaxs, Geom.point, Geom.smooth(method=:loess, smoothing=0.9), Geom.errorbar)
+#l1 = layer(hmdata[:x1], background)
+p3 = plot(x=0:23, y=hmdata3[:x1], ymin=ymins, ymax = ymaxs, Geom.point, Geom.smooth(method=:loess, smoothing=0.9), Geom.errorbar, light_panel)
 
 draw(SVG("Mean likelihood of stickiness by hour with error.svg", 10inch, 5inch), p3)
+draw(PNG("Mean likelihood of stickiness by hour with error.png", 10inch, 5inch), p3)
+
+hmdata4 = by(df, [:hour], x -> [mean(x[:gap]) std(x[:gap])])
+Ns = by(df, [:hour], x-> size(x)[1])
+ymins = hmdata4[:x1] .- (1.96 * Array(hmdata4[:x2]) ./ Array(sqrt(Ns[:x1])))
+ymaxs = hmdata4[:x1] .+ (1.96 * Array(hmdata4[:x2]) ./ Array(sqrt(Ns[:x1])))
+p4 = plot(x=0:23, y=hmdata4[:x1], ymin=ymins, ymax = ymaxs, Geom.point, Geom.smooth(method=:loess, smoothing=0.9), Geom.errorbar, light_panel)
+
+draw(SVG("Mean gap between microsessions by hour with error.svg", 10inch, 5inch), p4)
+draw(PNG("Mean gap between microsessions by hour with error.png", 10inch, 5inch), p4)
